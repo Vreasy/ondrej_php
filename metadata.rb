4d5bfe184f0e7bf7df2f5fb947cfc5ed67123640
Mauro Asprea <mauroasprea@gmail.com>
2016-11-08 17:13:53 +0100

    Remove `chef_version` not supported in  chef < 12

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs and maintains the Ondřej Surý Co-installable PHP version
 
 source_url 'https://github.com/brutuscat/ondrej_php'
 issues_url 'https://github.com/brutuscat/ondrej_php/issues'
-chef_version '>= 12.1'
+
 version '0.1.0'
 
 supports 'ubuntu', '>= 14.04'
